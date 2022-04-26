313195cba8569032a6155ea8690734035b5f026a
rishi <101212190+rworld2001@users.noreply.github.com>
2022-04-26 12:43:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ dis_width = 800
 dis_height = 600
  
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game using python by Daipayan Das')
+pygame.display.set_caption('Snake Game using python by Rajat Majumder')
  
 clock = pygame.time.Clock()
  
